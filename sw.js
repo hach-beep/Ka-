cd7e6977a7725af42ac67b0cfc09535fcfd3020b
hach-beep <haiyamkhalid@gmail.com>
2026-02-06 13:55:05 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+const cacheName = 'clinic-v1';
+const assets = ['./dr.html', './manifest.json', './doctor.jpg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
